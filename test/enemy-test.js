dc5c7fc92a8ec3e1fdda555a73dfc06681c38caf
kamos1 <keji.amos@gmail.com>
2017-03-20 19:26:43 -0600

    add semicolons, add enemy-test, es6 refactor

---
+++ test/enemy-test.js
@@ -0,0 +1,30 @@
+const assert = require('chai').assert;
+const Enemy = require('../lib/Enemy');
+
+describe('This will test the Enemy class functionality', () => {
+  it('should be a function', () => {
+    assert.isFunction(Enemy);
+  });
+
+  it('should create a new instance of Enemy', () => {
+    let enemy1 = new Enemy();
+    assert.isObject(enemy1);
+  });
+
+  it('should change enemy x value to 160', () => {
+    let enemy1 = new Enemy(150,50, 10, 10, 'green', 10);
+    enemy1.move();
+    assert.equal(enemy1.x, 160);
+  });
+
+  it('should change enemy x value to 0', () => {
+    let enemy1 = new Enemy(290,50, 10, 10, 'green', 11);
+    enemy1.move();
+    assert.equal(enemy1.x, 0);
+  });
+
+  it('should draw an enemy', () => {
+    let enemy1 = new Enemy(290,50, 10, 10, 'green', 11);
+    enemy1.draw();
+  });
+});
